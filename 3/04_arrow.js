3fe65ae20bbe0af6aa91fca54f982763dfc9c831
Medhankar Thamke <tmedhankar@gmail.com>
2024-09-07 14:31:01 +0530

    arrow

---
+++ 3/04_arrow.js
@@ -0,0 +1,8 @@
+// arrow function
+//simple arrow fun (explicit return)
+// const addNum = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// implicit return
+const addTwoNum = (num1, num2) => num1 + num2;
