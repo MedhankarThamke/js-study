b7c8fa86e73fe06c72993e7d6a5f80f82b1a3646
Medhankar Thamke <tmedhankar@gmail.com>
2024-09-07 13:35:22 +0530

    scope

--- 3/02_function.js
+++ 3/02_function.js
@@ -10,3 +10,18 @@ function one() {
   //   console.log(age);//cannot be accessed
 }
 one();
+
+// ------------interesting-------
+//we can declare the function in two methd
+
+//when we declare this way we can call the funct before the declaration
+
+// console.log(addOne(2));
+function addOne(num) {
+  return num + 1;
+}
+
+const addTwo = function (num) {
+  return num + 2;
+};
+// console.log(addTwo(3));
